443f536faf3610785584ec8928554553f582694d
jeenajose20 <jeenajose.chinnu@gmail.com>
2025-01-29 14:42:32 +0100

    Updated

--- Currency_API_Test/src/main/java/testCases/FixerAPITests.java
+++ Currency_API_Test/src/main/java/testCases/FixerAPITests.java
@@ -18,12 +18,14 @@ public class FixerAPITests {
 	private ExtentTest test;
 
 	public void initializeServiceCallwithValidAPI(String testmessage) {
+
 		test = ExtentReportManager.createTest(testmessage);
 		accessKey = ConfigUtil.get("access_key");
 		test.info("Access Key: " + accessKey);
 		if (accessKey.isEmpty()) {
-			test.fail("Invalid accesskey" + accessKey);
-			Assert.assertNotNull(null, "API key should not be null");
+			boolean b = accessKey.isEmpty();
+			test.fail("Access key is not supplied");
+			Assert.assertTrue(b, "Access key is not supplied");
 
 		} else {
 			test.pass("Valid access key" + accessKey);
@@ -38,11 +40,9 @@ public class FixerAPITests {
 		RestAssured.baseURI = ConfigUtil.get("base.url");
 		if (accessKey.isEmpty()) {
 			response = given().queryParam("symbols", symbols).when().get();
-			test.fail("Invalid access key");
+			test.fail("Access key is not supplied");
 		} else {
 			response = given().queryParam("access_key", accessKey).queryParam("symbols", symbols).when().get();
-			System.out.println(symbols);
-			System.out.println(accessKey);
 			test.pass("Request sent successfully");
 		}
 		test.info("Response code:" + response.asString());
@@ -50,10 +50,8 @@ public class FixerAPITests {
 	}
 
 	public void currencyCoversionIsInvoked(int expectedStatusCode) {
-		test.info("Expected Status code:" + expectedStatusCode);
-		Assert.assertEquals(response.getStatusCode(), expectedStatusCode, "Status code should match");
 		test.info("Actual Status code:" + response.getStatusCode());
-		System.out.println(response.getStatusCode());
+		Assert.assertEquals(response.getStatusCode(), expectedStatusCode, "Status code should match");
 		test.pass("Status code genereated successfully");
 	}
 
@@ -65,66 +63,61 @@ public class FixerAPITests {
 				response.jsonPath().getString("rates." + currency1));
 		Assert.assertNotNull("Rate for " + currency2 + " should be present",
 				response.jsonPath().getString("rates." + currency2));
-		System.out.println(currency1);
-		System.out.println(currency2);
 		test.pass("Conversion rates generated successfully");
 
 	}
-	
+
 	public void validateResponsewithSingleCurrency(String currency) {
-		 test.info("Rate for " + currency + " is"+response.jsonPath().getString("rates." + currency));
-		 Assert.assertNotNull("Rate for " + currency + " should be present", response.jsonPath().getString("rates." + currency));
-		 System.out.println(currency);
-		 test.pass("Conversion rates generated successfully");
+		test.info("Rate for " + currency + " is " + response.jsonPath().getString("rates." + currency));
+		Assert.assertNotNull("Rate for " + currency + " should be present",
+				response.jsonPath().getString("rates." + currency));
+		test.pass("Conversion rates generated successfully");
 	}
-	
-	public void initializeServiceCallwithInvalidAPI(){
+
+	public void initializeServiceCallwithInvalidAPI() {
 		test = ExtentReportManager.createTest("Request without an API key");
-		test.info("Access Key:");
-		accessKey=null;
-	    if(accessKey==null) {
-	    	test.info("Invalid accesskey has been passed");
-	    	Assert.assertNull(accessKey, "API key should be null");
-	  
-	    	
-	    }else {
-	    	test.info("Valid access key" + accessKey);
-	    	Assert.assertNull(accessKey, "API key should be null");
-	    }
+		accessKey = "";
+		if (accessKey.isEmpty()) {
+			boolean b = accessKey.isEmpty();
+			test.info("Acces key is not supplied");
+			Assert.assertTrue(b, "Acces key is not supplied");
+
+		} else {
+			test.info("Access key is supplied:" + accessKey);
+			Assert.assertTrue(false, "Acces key is supplied");
+		}
 	}
-	
-	public void generateCurrencyConvertInvalidAPIcall(String symbols){
+
+	public void generateCurrencyConvertInvalidAPIcall(String symbols) {
 		RestAssured.baseURI = ConfigUtil.get("base.url");
-	    if(accessKey==null) {
-	    	response = given().queryParam("symbols",symbols).when().get();
-	    	test.pass("Invalid access key");
-	    } else {
-	    	response = given().queryParam("access_key",accessKey).queryParam("symbols",symbols).when().get();
-	    	System.out.println(symbols);
-	    	System.out.println(accessKey);
-	    	test.fail("Request sent successfully");	    	
-	    }    
-	    test.info("Response code:"+response.asString());
+		if (accessKey.isEmpty()) {
+			response = given().queryParam("symbols", symbols).when().get();
+			test.pass("Request sent without Access key");
+		} else {
+			response = given().queryParam("access_key", accessKey).queryParam("symbols", symbols).when().get();
+			test.fail("Request sent successfully");
+		}
+		test.info("Response code:" + response.asString());
 	}
-	
+
 	public void validateInvalidKeyErrorcode(int errorCode) {
-		Assert.assertEquals(response.jsonPath().getInt("error.code"), errorCode, "Error code should match");
 		test.info("Error code:" + response.jsonPath().getInt("error.code"));
+		Assert.assertEquals(response.jsonPath().getInt("error.code"), errorCode, "Error code should match");
 	}
-	
+
 	public void validateInvalidAccesKeyErrorMessage(String expectedError) {
-		test.info("Expected Error: "+ expectedError);
-		System.out.println("Expected Error: "+ expectedError);
-		System.out.println(response.asString());
-		test.info("Error message: "+response.jsonPath().getString("error.info"));
-		Assert.assertTrue(response.asString().contains(expectedError), "Error message should contain: " + expectedError);
+		test.info("Expected Error: " + expectedError);
+		test.info("Error message: " + response.jsonPath().getString("error.info"));
+		Assert.assertTrue(response.asString().contains(expectedError),
+				"Error message should contain: " + expectedError);
 		test.pass("Error message generated succesfully");
 	}
-	
+
 	public void validateInvalidSymbolsErrorMessage(String expectedErrorField) {
-		test.info("Response "+ response.asString());
-		Assert.assertTrue(response.asString().contains(expectedErrorField), "Error message should mention: " + expectedErrorField);
-		test.info("Error message: "+response.jsonPath().getString("error.info"));
+		test.info("Response " + response.asString());
+		Assert.assertTrue(response.asString().contains(expectedErrorField),
+				"Error message should mention: " + expectedErrorField);
+		test.info("Error message: " + response.jsonPath().getString("error.info"));
 		test.pass("Error message generated succesfully");
 	}
 
